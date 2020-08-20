cba6815fc13e9e3deeaf367c882105ba2731ad76
U-1288 <U-1288@L9841.ibsplc.com>
2020-08-20 01:52:37 -0500

    First Commit

---
+++ LearnJava/src/classesandobjects/AccountHolderObjects.java
@@ -0,0 +1,34 @@
+package classesandobjects;
+
+public class AccountHolderObjects {
+	
+	public static void main(String[] args) {
+			
+		AccountHolder tom = new AccountHolder();
+		AccountHolder henry = new AccountHolder();
+		AccountHolder sarah = new AccountHolder();
+		
+		tom.firstName="Tom";
+		tom.lastName="Smith";
+		tom.age=21;
+		tom.accountBalance=10000;
+		tom.checkEligabilityForCreditCard();
+		System.out.println("Is Tom Eligable for Ceridt Card : "+tom.eligableForCreditCard);
+		
+		
+		//henry.firstName="Henry";
+		//henry.lastName="Hill";
+		henry.age=31;
+		henry.accountBalance=20000;
+		henry.checkEligabilityForCreditCard();
+		System.out.println("Is Henry Eligable for Ceridt Card : "+henry.eligableForCreditCard);
+		
+		sarah.firstName="Sarah";
+		sarah.lastName="Miller";
+		sarah.age=41;
+		sarah.accountBalance=30000;
+		sarah.checkEligabilityForCreditCard();
+		System.out.println("Is Sarah Eligable for Ceridt Card : "+henry.eligableForCreditCard);
+	} 
+
+}
---
+++ LearnJava/src/myfirstjavaprogram/AllPraiseToGod.java
@@ -0,0 +1,14 @@
+package myfirstjavaprogram;
+
+public class AllPraiseToGod {
+
+	public static void main(String[] args) {
+	
+	System.out.println("All Praise to God");
+	System.out.print("Let peace be with this nation ");
+	System.out.println("And peace be with you..!! ");
+	System.out.println("............... ");
+		
+	}
+
+}
